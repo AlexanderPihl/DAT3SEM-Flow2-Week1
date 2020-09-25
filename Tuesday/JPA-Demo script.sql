b185837a02d18a9015c70c168862a46316505fb9
Alexander Pihl <cph-as509@cphbusiness.dk>
2020-09-25 15:50:13 +0200

    removed tuesday from PART3

--- Tuesday/JPA-Demo script.sql
+++
@@ -1,8 +0,0 @@
--- Find navn og årgang samt svømmestile for alle personer
-
-use JPA_Demo;
-select p.NAME, p.YEAR, ss.STYLENAME from PERSON p
-inner join SWIMSTYLE_PERSON sp
-on p.P_ID = sp.persons_P_ID
-inner join SWIMSTYLE ss
-on sp.styles_ID = ss.ID;
