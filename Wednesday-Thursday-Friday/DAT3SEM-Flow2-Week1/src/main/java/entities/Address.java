b16845e5226c0b2179faa071691379e706a5b83c
Alexander Pihl <cph-as509@cphbusiness.dk>
2020-09-25 10:23:19 +0200

    address entity

---
+++ Wednesday-Thursday-Friday/DAT3SEM-Flow2-Week1/src/main/java/entities/Address.java
@@ -0,0 +1,65 @@
+package entities;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author alexa
+ */
+@Entity
+public class Address implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+    private String street;
+    private String zip;
+    private String city;
+
+    public Address() {
+    }
+
+    public Address(String street, String zip, String city) {
+        this.street = street;
+        this.zip = zip;
+        this.city = city;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getZip() {
+        return zip;
+    }
+
+    public void setZip(String zip) {
+        this.zip = zip;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+    
+}
